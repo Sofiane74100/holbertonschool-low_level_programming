be64b90a09b1048bc4cba414584cd45ca129c46a
Sofiane74100 <arfane.sofiane@gmail.com>
2024-02-26 12:38:11 +0100

    b

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -8,6 +8,17 @@
  */
 int main(void)
 {
+    
+    int _isupper(int c) {
+    if (c >= 'A' && c <= 'Z') {
+        return 1;
+    } else {
+        return 0;
+    }
+
+
+
+    
     char c;
 
     c = 'A;
