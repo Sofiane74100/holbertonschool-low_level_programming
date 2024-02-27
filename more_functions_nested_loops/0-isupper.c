a0665c9cfcb62f781c666865871117cacfc10b6d
Sofiane74100 <arfane.sofiane@gmail.com>
2024-02-27 17:43:43 +0100

    W

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * main - check the code.
@@ -8,12 +7,11 @@
 */
 
 
-int _isupper(int c)
+int __isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-}
-{
-		else
+	else
+		
 		return (0);
 	}
