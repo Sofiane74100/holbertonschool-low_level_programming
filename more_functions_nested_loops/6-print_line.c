7c8aa81e61008751a16d4f4df480a379fa0bc81b
Sofiane74100 <arfane.sofiane@gmail.com>
2024-02-29 15:12:14 +0100

    6.

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * print_line - draw a straight line with n underscores
+ * @n: number of underscores to put
+ */
+
+void print_line(int n)
+
+{
+
+int n;
+
+for (n = 0 ; n > 0 ; n++)
+
+if (n <= 0)
+
+_putchar (n = '_');
+
+_putchar (\n);
+
+}
