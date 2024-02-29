7fc32d94acded53177d1c22c39c05b480344aa08
Sofiane74100 <arfane.sofiane@gmail.com>
2024-02-29 15:24:39 +0100

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,17 +5,16 @@
  */
 
 void print_line(int n)
-
 {
 
 int n;
 
 for (n = 0 ; n < 0 ; n++)
 
-if (n <= 0)
+if (n < 0)
 
-_putchar (n = '_');
+_putchar ('_');
 
-_putchar (\n);
+_putchar ('\n');
 
 }
