b1767791ae5a05d2ea9f5afb1ed88d80e1886b13
Sofiane74100 <arfane.sofiane@gmail.com>
2024-03-04 00:21:16 +0100

    k

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be concatened
+ * @src: string to add
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+
+	dest[i + j] = '\0';
+
+	return (dest);
+}
