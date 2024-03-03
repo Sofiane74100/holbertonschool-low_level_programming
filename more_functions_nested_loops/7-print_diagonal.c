596abc212766246337481c1a42f58d0fbb5e8b54
Sofiane74100 <arfane.sofiane@gmail.com>
2024-03-03 01:38:04 +0100

    j

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * print_line - draw a straight line with n underscores
+ * @n: number of underscores to put
+ */
+
+void print _diagonal(int n)
+
+{
+
+int i;
+
+for (i = 0 ; i > 0 ; i++)
+
+if (n <= 0)
+
+_putchar ('\');
+
+_putchar ('\n');
+
+}
