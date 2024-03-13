9da2ec7d5f6c5e3773129472e4b81d3a72910236
Sofiane74100 <arfane.sofiane@gmail.com>
2024-03-13 15:24:29 +0100

    betty style

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,15 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
+ * create_array - create an array
+ * @c: caractere
  * @size: the size of the memory to print
  *
- * Return: Nothing.
+ * Return: table
  */
-
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
