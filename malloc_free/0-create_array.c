3bf175055d99b72fbc675f06c4afcadf7567fd10
Sofiane74100 <arfane.sofiane@gmail.com>
2024-03-13 15:17:42 +0100

    j

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	array = malloc(size * sizeof(char));
 
 	if (array == NULL)
-		return (NULL);
+		return (((void *)0));
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
