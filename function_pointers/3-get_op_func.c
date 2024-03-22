d8b9f257adc383bf47e8f94a0e719d43ebb60033
Sofiane74100 <arfane.sofiane@gmail.com>
2024-03-22 22:29:06 +0100

    l

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,13 +1,13 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 /**
- * get_op_func - contain the function that selects the correct function
- * @s: pointeur
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
  *
- * Return: NULL
-*/
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -20,13 +20,14 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	while (i <= 4)
+	i = 0;
+
+	while (ops[i].op)
 	{
-		if (*ops[i].op == *s)
-		{
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
-		}
 		i++;
 	}
+
 	return (NULL);
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
-#ifndef HOLBERTON_H_FILE
-#define HOLBERTON_H_FILE
+#ifndef _HEADER_
+#define _HEADER_
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
 #endif
