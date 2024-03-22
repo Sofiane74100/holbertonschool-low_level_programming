6958382cb7c404d9d56b48aae0fc435742384e10
Sofiane74100 <arfane.sofiane@gmail.com>
2024-03-22 15:07:09 +0100

    Write a function that prints anything.
    Prototype: void print_all(const char * const format, ...);
    where format is a list of types of arguments passed to the function
    c: char
    i: integer
    f: float
    s: char * (if the string is NULL, print (nil) instead
    any other char should be ignored
    see example
    You are not allowed to use for, goto, ternary operator, else, do ... while
    You can use a maximum of
    2 while loops
    2 if
    You can declare a maximum of 9 variables
    You are allowed to use printf
    Print a new line at the end of your function

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,89 +1,48 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-/**
- * print_char - Prints a char.
- * @arg: A list of arguments pointing to the character
- */
-void print_char(va_list arg)
-{
-	char letter;
 
-	letter = va_arg(arg, int);
-	printf("%c", letter);
-}
 /**
- * print_int - Prints an int.
- * @arg: A list of arguments pointing to the integer
- */
-void print_int(va_list arg)
-{
-	int letter;
-
-	letter = va_arg(arg, int);
-	printf("%d", letter);
-}
-/**
- * print_float - Prints a float.
- * @arg: A list of arguments pointing to the float
- */
-void print_float(va_list arg)
-{
-	float decimal_number;
-
-	decimal_number = va_arg(arg, double);
-	printf("%f", decimal_number);
-}
-/**
- * print_string - Prints a string.
- * @arg: A list of arguments pointing to the string
- */
-void print_string(va_list arg)
-{
-	char *str;
-
-	str = va_arg(arg, char *);
-
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-/**
- * print_all - Prints anything
- * @format: string of caractere who represent all argument
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-	va_list arg;
-	int index = 0, index2 = 0;
-	char *separator = "";
-	op_t op[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string},
-		{NULL , NULL}
-	};
-	va_start(arg, format);
+	int i = 0;
+	char *str, *sep = "";
 
-	while (format != NULL && (format[index]) != '\0')
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
 	{
-		index2 = 0;
-
-		while (index2 <= 3 && format[index] != *(op[index2]).ap)
-			index2++;
-		if (index2 <= 3)
+		while (format[i])
 		{
-			printf("%s", separator);
-			op[index2].f(arg);
-			separator = ", ";
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		index++;
 	}
-	printf("\n");
 
-	va_end(arg);
+	printf("\n");
+	va_end(list);
 }
