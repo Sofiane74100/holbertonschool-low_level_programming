b49834ec9f42e6f68c986edd38ecf6e455a3893f
Sofiane74100 <arfane.sofiane@gmail.com>
2024-03-22 22:41:52 +0100

    k

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,22 +1,12 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
+
+#include <stdio.h>
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-/**
- * struct op - new struct type defining a printer.
- * @op: representing a data type.
- * @f: function pointers
- */
-typedef struct op
-{
-	char *ap;
-	void (*f)(va_list arg);
-
-} op_t;
 
 #endif
